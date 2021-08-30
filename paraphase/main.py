d377139902c12744700b5606496586b4d653703a
Cyndie Russeeawon <crusseeaw@gmail.com>
2021-08-30 18:13:01 +0200

    Edited create_parser() for more args

--- paraphase/main.py
+++ paraphase/main.py
@@ -1,26 +1,61 @@
 from __future__ import print_function
-import configparser
+# import configparser
+import argparse
 import yaml
 from yaml.loader import SafeLoader
+import sys
 
 def create_parser(argv=None):
 	"""
-	Create a ArgumentParser object.
+	Create an ArgumentParser object.
 
 	"""
 
-	# p0 = argparse.ArgumentParser(
-	# 		description=__doc__,
-	# 		formatter_class=argparse.RawDescriptionHelpFormatter,
-	# 		add_help=False)
-	p0 = argparse.ArgumentParser()
-	
-	p = p0.add_argument_group("Input")
-	p.add_argument("--ms", dest="ms", help="Name of measurement set", type=str, action="append")
-	p.add_argument("--sky_model", dest="sky_model", type=str, help="Tigger lsm file", action="append")
-	p.add_argument("--cfg", dest="cfg", type=argparse.FileType(mode='r'))
+	if argv is None:
+		argv = sys.argv
 
-	return p0
+	pconf = argparse.ArgumentParser(description=__doc__,
+								formatter_class=argparse.RawDescriptionHelpFormatter,
+								add_help=False)
+
+	pconf.add_argument("-c", "--conf_file", help="Specify configuration file", metavar="FILE", default="default.yml")
+	args, remaining_argv = pconf.parse_known_args()
+
+	default = {}
+
+	if args.conf_file:
+		# c = ConfigParser.SafeConfigParser()
+		config = configparser.ConfigParser()
+		config.read([args.conf_file])
+		default.update(dict(config.items("data")))
+		default.update(dict(config.items("g")))
+		default.update(dict(config.items("sky")))
+		default.update(dict(config.items("out")))
+
+
+	#Parsing the rest of the arguments.
+	p = argparse.ArgumentParser(parents=[pconf])
+
+	p.set_defaults(**default)
+	#about gains
+	p1 = p.add_argument_group("g")
+	p1.add_argument("--g-save-to", dest="save-to", type=str, help="Save gains to this address")
+	p1.add_argument("--g-time-int", dest="time-int", type=int, help="Size of solution time interval")
+	p1.add_argument("--g-freq-int", dest="freq-int", type=int, help="Size of solution frequency interval")
+	#about data
+	p2 = p.add_argument_group("data")
+	p2.add_argument("--data-ms", dest="ms", help="Name of measurement set", type=str, action="append")
+	p2.add_argument("--data-column", dest="column", type=str, help="Name of MS column to read for data")
+	#about sky
+	p3 = p.add_argument_group("sky")
+	p3.add_argument("--sky-model", dest="sky-model", type=str, help="Tigger lsm file", action="append")
+	#about output
+	p4 = p.add_argument_group("out")
+	p4.add_argument("--out-writeto", dest="out-writeto", type=str, help="Write to output MS column")
+	
+	args = p.parse_args(remaining_argv)
+
+	return args
 
 
 def main(debugging=False):
@@ -29,7 +64,7 @@ def main(debugging=False):
 
 	"""
 
-	args = create_parser().parse_args()
+	args = create_parser()
 	
 
 
