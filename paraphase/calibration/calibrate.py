4d985a57dd5b1d42f5466112f3f86f6175ebcade
Cyndie Russeeawon <crusseeaw@gmail.com>
2021-09-07 12:30:04 +0200

    Redefined MasterMachine class

--- paraphase/calibration/calibrate.py
+++ paraphase/calibration/calibrate.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 import numpy as np
-
+from abc import ABCMeta
 
 #Maybe treat this like the MasterMachine class from CubiCal?!
-class ParametrisedPhase:
+class MasterMachine(metaclass=ABCMeta):
 	"""
-	Implements parametrised phase-only solver.
+	Provides base class for parametrised phase-only solver.
+	And, lays the basic requirements for all machines.
 
 	"""
 
@@ -13,9 +14,24 @@ class ParametrisedPhase:
 		"""
 		Initialises a phase-only parametrised gain solver.
 
-		**about arguments
+		Args:
+			args (dict or namespace?)
+			....
 
 		"""
 
 		self.args = args
+
+	def compute_jhj(self):
+		return NotImplementedError
+
+
+	def compute_update(self):
+		return NotImplementedError
+
+	def compute_residual(self):
+		return NotImplementedError
+
+	def apply_gains(self):
+		return NotImplementedError
 
