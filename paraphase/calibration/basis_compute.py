a7b60381769e4765e78b89093b6bcde7f05d6791
Cyndie Russeeawon <crusseeaw@gmail.com>
2021-09-22 17:26:09 +0200

    Edited get_basis_cov() to accept bparams

--- paraphase/calibration/basis_compute.py
+++ paraphase/calibration/basis_compute.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def _make_basis_vec(n_param, l_s, m_s):
+def make_basis_vec(n_param, l_s, m_s):
     """
     Generating the basis polynomial to compute the phase equation. Right now, 
     it is of the form [1, l_s, m_s, l_s**2, m_s**2, ...] with l_s and m_s being 
@@ -18,34 +18,36 @@ def _make_basis_vec(n_param, l_s, m_s):
     
     return np.insert(main_vec, 0, [1])
 
-# def _get_basis(n_param, sources):
-#     """
-#     Get basis matrix of shape (n_params, n_dir). Both l and m, which represent the
-#     direction cosine coordinates of the sources are each vectors of length n_dir.
+def get_basis_poly(n_par, sources):
+    """
+    Get basis matrix of shape (n_params, n_dir). Both l and m, which represent the
+    direction cosine coordinates of the sources are each vectors of length n_dir.
     
-#     """
+    """
 
-#     #Get the dimension and the direction cosines.
-#     l = sources[:, 1]
-#     m = sources[:, 2]
+    #Get the dimension and the direction cosines.
+    l = sources[:, 1]
+    m = sources[:, 2]
 
-#     #Get "n_dir" dimension.
-#     n_dir = sources.shape[0] #len(l)
-#     basis = np.zeros((n_param, n_dir))
+    #Get "n_dir" dimension.
+    n_dir = sources.shape[0] #len(l)
+    basis = np.zeros((n_par, n_dir))
 
-#     for s in range(n_dir):
-#         basis[:, s] = _make_basis_vec(n_param, l[s], m[s])
+    for s in range(n_dir):
+        basis[:, s] = make_basis_vec(n_par, l[s], m[s])
 
-#     return basis
+    return basis
 
-def _get_basis(sources):
+def get_basis_cov(bparams, sources):
     """
     Get basis matrix >>> covariance matrix >>> Cholesky decomposition.
-    
+    recall bparams = {"n_par": n_dir, "sigmaf": options.sigmaf, "lscale": options.lscale}
+
     """
 
     #Get covariance matrix.
-    K = _squared_exp(sources, sources)
+    #(sources, sources)? (x, xp)? maybe needs tweaking?
+    K = _squared_exp(sources, sources, bparams["sigmaf"], bparams["lscale"])
 
     #
     jitter = 1e-6
@@ -57,7 +59,7 @@ def _get_basis(sources):
 
     return L
 
-def _squared_exp(x, xp, sigmaf=1000, l=0.1):
+def squared_exp(x, xp, sigmaf, l):
     """
     The function returns a covariance matrix using the squared 
     exponential (SE) kernel.
@@ -81,3 +83,17 @@ def _squared_exp(x, xp, sigmaf=1000, l=0.1):
             C[i, j] = sigmaf**2*np.exp(-(1/2*l**2)*((x[:, 1][i] - xp[:, 1][j])**2 + (x[:, 2][i] - xp[:, 2][j])**2))
             
     return C
+
+
+def basis_compute(msrcs, bparams, solver):
+    """
+    The function computes a basis given the specifications.
+    params is n_par when using a ppoly gtype.
+    params is n_dir, sigmaf, l and more when using a pcov gtype.
+
+    """
+
+    if solver == "ppoly":
+        return get_basis_poly(bparams["n_par"], msrcs)
+    elif solver == "pcov":
+        return get_basis_cov(bparams, msrcs)
