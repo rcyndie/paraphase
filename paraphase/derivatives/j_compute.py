bd64237f166d62e1a54fe950069e8b6cf4fcc51f
landmanbester <lbester@ska.ac.za>
2021-11-04 17:30:49 +0200

    Explicit jacobain construction for test

--- paraphase/derivatives/j_compute.py
+++ paraphase/derivatives/j_compute.py
@@ -13,7 +13,7 @@ def j_compute(data_arr, model_arr, gains, gparams, alpha, basis, datadiag=None):
 
 	#Initialise Jacobian.
 	jac = np.zeros(data_arr.shape+alpha.shape, dtype=data_arr.dtype)
-	
+
 	for t in range(n_tim):
 		tt = t//n_timint
 		for f in range(n_fre):
@@ -34,5 +34,46 @@ def j_compute(data_arr, model_arr, gains, gparams, alpha, basis, datadiag=None):
 								# 	jac[t, f, p, q, k, k, tt, ff, q, param, k] += -dphidalpha * gains[d, tt, f, p, k]* model_arr[d, t, f, p, q, k, k] * np.conj(gains[d, tt, f, q, k].T)
 					# Set [q,p] element as conjugate of [p,q].
 					jac[t, f, q, p] = np.conj(jac[t, f, p, q])
+
+	return np.reshape(jac, (data_arr.size, alpha.size))
+
+def j_compute_slow(data_arr, model_arr, freq, alpha, fprofile, basis, l, m):
+	"""
+	Returns the Jacobian.
+
+	"""
+
+	ntime, nchan, nant, _, ncor = data_arr.shape
+	nsource = model_arr.shape[-1]
+	ntimeint, nchanint, nant, ncorr, npar = alpha.shape
+
+	#Initialise Jacobian.
+	jac = np.zeros(data_arr.shape+alpha.shape, dtype=data_arr.dtype)
+
+	for t in range(ntime):
+		for f in range(nchan):
+			fprof = fprof(freq[f])
+			for p in range(nant):
+				for q in range(nant):
+					for c in range(ncorr):
+						# end of data axes
+						# start of param axes
+						for tt in range(ntimeint):
+							for ff in range(nchanint):
+								for ant in range(nant):
+									for cc in range(ncorr):
+										for par in range(npar):
+											for s in range(nsource):
+												#Get partial derivative of the phase.
+												if ant==p and cc==c and tt==t//ntimeint and ff==f//nchanint:
+													dphidalpha = 1.0j*fprof*basis(l[s], m[s], alpha[tt, ff, ant, cc])
+													gp = np.exp(dphidalpha)
+													gq = 1.0j*fprof*basis(l[s], m[s], alpha[tt, ff, q, cc])
+													jac[t, f, p, q, c, tt, ff, ant, cc, par] += dphidalpha * gp * model_arr[t, f, p, q, c, s] * np.conj(gq)
+												elif ant==q and cc==c and tt==t//ntimeint and ff==f//nchanint:
+													dphidalpha = 1.0j*fprof*basis(l[s], m[s], alpha[tt, ff, ant, cc])
+													gp = 1.0j*fprof*basis(l[s], m[s], alpha[tt, ff, p, cc])
+													gq = np.exp(dphidalpha)
+													jac[t, f, p, q, c, tt, ff, ant, cc, par] += -dphidalpha*gp*model_arr[t, f, p, q, c, s] * np.conj(gq)
 
 	return np.reshape(jac, (data_arr.size, alpha.size))
--- tests/test_jacobian.py
+++ tests/test_jacobian.py
@@ -1,4 +1,51 @@
 import pytest
+from paraphase.derivatives.j_compute import j_compute_slow
 
 def test_jacobian():
-    pass
+    # defined coordinates
+    nchan = 3
+    freq = np.linspace(0.5, 1.5, nchan)
+    ntime = 2
+    time = np.linspace(0, 1, ntime)
+    nsource = 3
+    lcoord = 0.1 * np.random.randn(3)
+    mcoord = 0.1 * np.random.randn(3)
+
+    # freq profile
+    def fprofile(v):
+        return 1./v
+
+    # For nt_int = 1 and nf_int 1 we have
+    # (nant - 1)/2 >= ndir
+    nant = 7
+
+    # parameters
+    nparam = 3
+    alpha = np.random.randn(ntime, nfreq, nant, ncorr, nparam)
+    def basis(l, m, alp):
+        return 1*alp[0] + l*alp[1] + m*alp[2]
+
+    # model vis
+    ncorr = 1
+    model_arr = (np.random.randn(ntime, nchan, nant, nant, ncorr, nsource) +
+                 1j*np.random.randn(ntime, nchan, nant, nant, ncorr, nsource))
+
+    # vis
+    vis_arr = np.zeros((ntime, nchan, nant, nant, ncorr), dtype=np.complex128)
+    for t in range(ntime):
+        for f in range(nchan):
+            for p in range(nant):
+                for q in range(nant):
+                    for c in range(ncorr):
+                        fprof = fprofile(freq[f])
+                        for s in range(nsource):
+                            phasep = basis(lcoord[s], mcoord[s], alpha[t, f, p, c])
+                            phaseq = basis(lcoord[s], mcoord[s], alpha[t, f, q, c])
+                            vis_arr[t, f, p, q, c] += (np.exp(1j*fprof*phasep) *
+                                                       model_arr[t, f, p, q, c, s] *
+                                                       np.exp(-1j*fprof*phaseq))
+
+    # evaluate jacobian
+    jac = j_compute_slow(data_arr, model_arr, freq, alpha, fprofile, basis, l, m)
+
+    # test against finite differences
