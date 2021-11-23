c38912f5a394cb2b5966bcefa217e13b10e7ff09
Cyndie Russeeawon <crusseeaw@gmail.com>
2021-11-23 12:16:46 +0200

    Evaluation of residuals_compute() without gains

--- paraphase/calibration/residuals_compute.py
+++ paraphase/calibration/residuals_compute.py
@@ -1,7 +1,7 @@
 import numpy as np
 from paraphase.format_1D2D import get_xxyy
 
-def residuals_compute(data_arr, model_arr, gains, datadiag=False):
+def residuals_compute(data_arr, model_arr, lcoord, mcoord, alpha, chan_freq, freqf, basis, datadiag=False):
     """
     The function returns difference between model_arr and data_arr
     at this instance.
@@ -10,23 +10,32 @@ def residuals_compute(data_arr, model_arr, gains, datadiag=False):
 
     #Initialise residuals.
     residuals = data_arr.copy()
-
-    n_dir, n_timint, n_fre, n_ant, n_ccor = gains.shape
-    n_timint = gains.shape[1]
+    
+    n_timint, n_freint, n_ant, n_ccor, _ = alpha.shape
     n_tim = data_arr.shape[0]
+    n_fre = data_arr.shape[1]
+    n_dir = model_arr.shape[0]
 
+    t_int = n_tim//n_timint
+    f_int = n_fre//n_freint
+    
     for t in range(n_tim):
-        tt = t//n_timint
+        tt = t//t_int
         for f in range(n_fre):
+            ff = f//f_int
+            fprof = freqf(chan_freq[f])
             for p in range(n_ant):
                 for q in range(p): #note only doing this for q < p
                     for d in range(n_dir):
                         for k in range(n_ccor):
                             #Subtract model for each direction.
+                            L = basis(lcoord[d], mcoord[d])
+                            gp = np.exp(1j * fprof * L.dot(alpha[tt, ff, p, k]))
+                            gq = np.exp(1j * fprof * L.dot(alpha[tt, ff, q, k]))
                             if datadiag:
-                                residuals[t, f, p, q, k] -= gains[d, tt, f, p, k] * model_arr[d, t, f, p, q, k] * np.conj(gains[d, tt, f, q, k].T)
+                                residuals[t, f, p, q, k] -= gp * model_arr[d, t, f, p, q, k] * np.conj(gq)
                             else:
-                                residuals[t, f, p, q, k, k] -= gains[d, tt, f, p, k] * model_arr[d, t, f, p, q, k, k] * np.conj(gains[d, tt, f, q, k].T)
+                                residuals[t, f, p, q, k, k] -= gp * model_arr[d, t, f, p, q, k, k] * np.conj(gq)
 
                     residuals[t, f, q, p] = np.conj(residuals[t, f, p, q])
 
