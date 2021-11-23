b001ca847999b93ef05640ada85785b95775c7e7
Cyndie Russeeawon <crusseeaw@gmail.com>
2021-11-23 13:09:00 +0200

    Added explicit JHJ implementation

--- paraphase/derivatives/jhj_compute.py
+++ paraphase/derivatives/jhj_compute.py
@@ -7,7 +7,7 @@ def jhj_compute(jac, jh, alpha):
 	"""
 
 	#
-	n_timint, n_freint, n_ant, n_par, n_ccor = alpha.shape
+	n_timint, n_freint, n_ant, n_ccor, n_par = alpha.shape
 	n_parccor = n_par*n_ccor
 
 	##Initialise jhj.
@@ -18,3 +18,34 @@ def jhj_compute(jac, jh, alpha):
 																			jac[:, k*n_parccor:(k+1)*n_parccor])
 
 	return jhj
+
+
+def jhj_compute_slow(jac, jh, alpha):
+	"""
+	Evaluates JHJ explicitly.
+	Remember this is not a block diagonal approximation. 
+
+	"""
+
+	#
+	n_timint, n_freint, n_ant, n_ccor, n_par = alpha.shape
+	n_tim = jac.shape[0]
+	n_fre = jac.shape[1]
+
+	#Initialise JHJ.
+	jhj = np.zeros((alpha.shape+alpha.shape), dtype=jac.dtype)
+
+	for tt in range(n_timint):
+		for ff in range(n_freint):
+			for ant in range(n_ant):
+				for cc in range(n_ccor):
+					for par in range(n_par):
+						for t in range(n_tim):
+							for f in range(n_fre):
+								for p in range(n_ant):
+									for q in range(n_ant):
+										for c in range(n_ccor):
+											jhj[tt, ff, ant, cc, par, tt, ff, ant, cc, par] += jh[tt, ff, ant, cc, par, t, f, p, q, c] * jac[t, f, p, q, c, tt, ff, ant, cc, par]
+
+	return jhj
+
